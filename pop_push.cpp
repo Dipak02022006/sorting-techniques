1694aa3553649b495f52569febccedf1a8f788a2
Dipak02022006 <deepakbhosale832@gmail.com>
2026-03-05 09:19:41 +0530

    Add stack implementation in pop_push.cpp
    Implement a stack class with push, pop, and display methods.

---
+++ pop_push.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 5
+
+class Stack {
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    void push(int x) {
+        if (top == MAX - 1) {
+            cout << "Stack Overflow" << endl;
+        } else {
+            top++;
+            arr[top] = x;
+            cout << x << " pushed into stack" << endl;
+        }
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack Underflow" << endl;
+        } else {
+            cout << arr[top] << " popped from stack" << endl;
+            top--;
+        }
+    }
+
+    void display() {
+        if (top == -1) {
+            cout << "Stack is empty" << endl;
+        } else {
+            cout << "Stack elements are: ";
+            for (int i = top; i >= 0; i--) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    s.pop();
+    s.display();
+
+    return 0;
+}
